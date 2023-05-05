e6c43c091081d6ad33d70170e8cec7ad1be9c737
Fernando Valenzuela <99852078+FernValenzuela@users.noreply.github.com>
2023-05-04 20:29:51 -0500

    Update App.js
    Added several actions to APP.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useReducer } from "react"
 import "./styles.css"
 
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation' ,
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, { type, payload })  {
+
+}
+
 function App()  {
+  const [{ currentOperand, previousOperand }, operation ] = useReducer(reducer, {})
     return (
-      <><div className="calculator-grid">
+      <div className="calculator-grid">
         <div className="output">
-          <div className="previous-operand"></div>
-          <div className="current-operand"></div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+          <div className="current-operand">{currentOperand}</div>
         </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
