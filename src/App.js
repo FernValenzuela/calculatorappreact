407257c2b4734954faf49dd2ed4b7962ed5ec5b8
Fernando Valenzuela <99852078+FernValenzuela@users.noreply.github.com>
2023-05-06 14:17:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,14 @@ export const ACTIONS = {
 
 function reducer(state, { type, payload })  {
 switch (type)  {
-  case ACTIONS.ADD_DIGIT:
+case ACTIONS.ADD_DIGIT:
+    if (state.overwrite) {
+      return {
+        ...state,
+        currentOperand: payload.digit,
+        overwrite: false,
+      }
+    }
     if (payload.digit === "0" && state.currentOperand === "0") {
       return state
     }
@@ -26,7 +33,7 @@ switch (type)  {
       ...state,
       currentOperand: '${state.currentOperand || ""}${payload.digit}',
     }
-    case ACTIONS.CHOOSE_OPERATION:
+case ACTIONS.CHOOSE_OPERATION:
       if (state.currentOperand == null && state.previousOperand == null) {
         return state
       }
@@ -37,6 +44,12 @@ if(state.currentOperand == null) {
     operation: payload.operation
   }
 }
+
+      if(state.currentOperand == null) {
+        return {
+          .state, operation: payload.operation
+        }
+      }
 
       if (state.previousOperand == null) {
         return {
@@ -53,8 +66,36 @@ if(state.currentOperand == null) {
         operation: payload,operation,
         currentOperand: null
       }
-    case ACTIONS.CLEAR:
+case ACTIONS.CLEAR:
       return {}
+case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) { 
+        return {
+          ...state,
+          overwrite:false,
+          currentOperand:null
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null }
+      }
+
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      }
+case ACTIONS.EVALUATE:
+      if (state.operation == null || state.currentOperand == null || state.previousOperand == null) {
+        return state
+      }
+
+      return {
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        currentOperand: evaluate(state),
+      }
   }
 }
 
@@ -81,6 +122,16 @@ function evaluate({ currentOperand, previousOperand, operation}) {
   return computation.toString()
 }
 
+const INTEGER_FORMATTER = Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+function formatOperand(operand) {
+  if (operand == null) return
+  const [ integer, decimal] = operand.split('.')
+  if (decimal == null) return INTEGER_FORMATTER.format(integer)
+  return '${INTEGER_FORMATTER.format(integer)}.${decimal}'
+}
+
 function App()  {
   const [{ currentOperand, previousOperand }, operation ] = useReducer(reducer, {})
 
@@ -88,11 +139,15 @@ function App()  {
     return (
       <div className="calculator-grid">
         <div className="output">
-        <div className="previous-operand">{previousOperand} {operation}</div>
-          <div className="current-operand">{currentOperand}</div>
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+          <div className="current-operand">{formatOperand(currentOperand)}</div>
         </div>
-        <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
-        <button>DEL</button>
+        <button className="span-two" 
+        onClick={() => dispatch({ type: ACTIONS.CLEAR})}
+        >
+          AC
+        </button>
+        <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT})}>DEL</button>
         <OperationButton operation="÷"dispatch={dispatch} />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
@@ -108,7 +163,10 @@ function App()  {
         <OperationButton operation="-"dispatch={dispatch} />
         <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
-        <button className="span-two">=</button>
+        <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE})}
+        >
+          =
+        </button>
       </div>
     )
 }
